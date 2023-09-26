6634d2b48c3747b861f092ec44e486b31b0a70cd
Musharrat711 <106244431+Musharrat711@users.noreply.github.com>
2023-09-26 17:08:21 +0530

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,196 @@
+class Questions:
+    GK_Questions = ["What is the capital city of Australia?\na) Sydney\nb) Melbourne\nc) Canberra\nd) Brisbane",
+                    "Who painted the famous artwork \"Mona Lisa\"?\na) Vincent van Gogh\nb) Pablo Picasso\nc) "
+                    "Leonardo da Vinci\nd) Michelangelo\n",
+                    "What is the chemical symbol for the element gold?\na) Au\nb) Ag\nc) Fe\nd) Hg\n",
+                    "Which planet is known as the \"Red Planet\"?\na) Venus\nb) Mars\nc) Jupiter\nd) Saturn\n",
+                    "In which country would you find the Eiffel Tower?\na) Germany\nb) France\nc) Italy\nd) "
+                    "Spain\n",
+                    "Who wrote the play \"Romeo and Juliet\"?\na) William Shakespeare\nb) Charles Dickens\nc) "
+                    "Jane Austen\nd) J.R.R. Tolkien\n",
+                    "What is the largest organ in the human body?\na) Liver\nb) Heart\nc) Brain\nd) Skin\n",
+                    "Which famous scientist developed the theory of relativity?\na) Isaac Newton\nb) Albert "
+                    "Einstein\nc) Galileo Galilei\nd) Nikola Tesla\n",
+                    "Which ocean is the largest in the world?\na) Atlantic Ocean\nb) Indian Ocean\nc) Arctic "
+                    "Ocean\nd) Pacific Ocean\n",
+                    "Who is the current President of the United States?\na) Joe Biden\nb) Barack Obama\nc) "
+                    "Donald Trump\nd) George W. Bush\n"]
+
+    gk_answers = ['c:  Canberra', 'c: Leonardo da Vinci', 'a: Au', 'b: Mars', 'b: France', 'a: William Shakespeare',
+                  'd: Skin', 'b: Albert Einstein', 'd: Pacific Ocean']
+
+    Music = ["Which musical instrument is commonly associated with Hindustani classical music?\na) Sitar\nb) "
+             "Tabla\nc) Veena\nd) Mridangam\n",
+             "Who is known as the \"Nightingale of India\"?\na) Lata Mangeshkar\nb) Asha Bhosle\nc) Shreya "
+             "Ghoshal\nd) KishoreKumar\n",
+             "Which musical mode is often used in Indian classical music?\na) Bhairavi\nb) Todi\nc) Kafi\nd) Yaman\n",
+             "Which percussion instrument is widely used in Carnatic music?\na) Ghungroo\nb) Ghatam\nc) Dholak\nd) "
+             "Santoor\n",
+             "Who composed the famous song \"Vande Mataram,\" which later became the national song of India?\na) "
+             "Rabindranath Tagore\nb) Swami Vivekananda\nc) Bankim Chandra Chattopadhyay\nd) Subramania Bharati\n",
+             "Which genre of music originated in the state of Punjab and is characterized by lively beats and "
+             "energetic dance?\na) Ghazal\nb) Qawwali\nc) Bhangra\nd) Kathak\n",
+             "Which renowned Indian musician is credited with popularizing the sitar globally?\na) Ravi Shankar\nb) "
+             "Zakir Hussain\nc) Hariprasad Chaurasia\nd) Ali Akbar Khan\n",
+             "Which musical scale is traditionally associated with the raga \"Mohanam\" in Carnatic music?\na) "
+             "Bilawal\nb) Bhairavi\nc) Kalyani\nd) Sankarabharanam\n",
+             "Which Indian playback singer is known for his soulful renditions in Bollywood movies and has been "
+             "honored with the Bharat Ratna?\na) Kishore Kumar\nb) Mohd Rafi\nc) Arijit Singh\nd) Lata Mangeshkar\n",
+             "What is the traditional rhythmic cycle or beat cycle called in Indian classical music?\na) Taal\nb) "
+             "Tala\nc) Laya\nd) Sangeet\n"]
+
+    mk_ans = ["a: Sitar", "a: Lata Mangeshkar", "d: Yaman", "b: Ghatam", "c: Bankim Chandra Chattopadhyay",
+              "c: Bhangra",
+              "c: Ravi Shankar", "c: Kalyani", "d: Lata Mangeshkar", 'b: Tala']
+
+    Movies = ["Who directed the Bollywood movie \"Dilwale Dulhania Le Jayenge\"?\na) Karan Johar\nb) Aditya "
+              "Chopra\nc) Sanjay Leela Bhansali\nd) Farhan Akhtar\n",
+              "Which actor played the lead role in the Bollywood movie \"Dangal\"?\na) Shah Rukh Khan\nb) Aamir "
+              "Khan\nc) Salman Khan\nd) Akshay Kumar\n",
+              "Which Bollywood movie won the Best Picture award at the 65th Filmfare Awards?\na) Gully Boy\nb) Uri: "
+              "The Surgical Strike\nc) Andhadhun\nd) Raazi\n",
+              "Who directed the Bollywood movie \"Bajirao Mastani\"?\na) Sanjay Leela Bhansali\nb) Rohit Shetty\nc) "
+              "Zoya Akhtar\nd) Imtiaz Ali\n",
+              "Which movie features the iconic song \"Mehndi Laga Ke Rakhna\"?\na) Kabhi Khushi Kabhie Gham\nb) "
+              "Dilwale Dulhania Le Jayenge\nc) Dil To Pagal Hai\nd) Kuch Kuch Hota Hai\n",
+              "Who played the character of Simran in the Bollywood movie \"Dilwale Dulhania Le Jayenge\"?\na) "
+              "Kajol\nb) Madhuri Dixit\nc) Kareena Kapoor Khan\nd) Deepika Padukone\n",
+              "Which actor portrayed the character of Munna Bhai in the Bollywood movie \"Munna Bhai MBBS\"?\na) "
+              "Aamir Khan\nb) Shah Rukh Khan\nc) Salman Khan\nd) Sanjay Dutt\n",
+              "Which Bollywood movie features the character of Chulbul Pandey?\na) Dabangg\nb) Kabir Singh\nc) "
+              "Padmaavat\nd) Gully Boy\n",
+              "Who directed the Bollywood movie \"3 Idiots\"?\na) Rajkumar Hirani\nb) Anurag Kashyap\nc) Anurag "
+              "Basu\nd) Farhan Akhtar\n",
+              "Which Bollywood movie won the Best Film award at the 66th National Film Awards?\na) Uri: The Surgical "
+              "Strike\nb) Badhaai Ho\nc) Andhadhun\nd) Padmaavat"]
+
+    mov_ans = ["b: Aditya Chopra", "b: Aamir Khan", "a: Gully Boy", "a: Sanjay Leela Bhansali",
+               "b: Dilwale Dulhaniya Le Jayenge", "a: Kajol", "d: Sanjay Dutt", "a: Dabangg", "a: Rajkumar Hirani",
+               "c: Andhadhun"]
+
+    Animals = ["Which animal is known for its black and white stripes\n?a) Zebra\nb) Cheetah\nc) Giraffe\nd) Lion\n"
+               "Which bird is known for its ability to mimic human speech?\na) Parrot\nb) Owl\nc) Penguin\nd) Peacock\n"
+               "Which animal is the largest land mammal?a) Elephant\nb) Rhino\nc) Hippopotamus\nd) Giraffe\n"
+               "Which animal is known for its ability to change color to match its surroundings?\na) Chameleon\nb) "
+               "Gecko\nc) Iguana\nd) Komodo dragon\n"
+               "Which Bird is capable of flying backwards?\na) Hummingbird\nb) Eagle\nc) Sparrow\nd) Pigeon\n"
+               "Which animal is known for its long neck and spots?\na) Giraffe\nb) Zebra\nc) Cheetah\nd) Kangaroo\n"
+               "Which animal has the longest lifespan?\na) Galapagos tortoise\nb) Blue whale\nc) African elephant\nd) "
+               "Greenland shark\n"
+               "Which animal is known for building intricate dams and lodges?\na) Beaver\nb) Otter\nc) Platypus\nd) "
+               "Muskrat\n"
+               "Which animal is the fastest land mammal?\na) Cheetah\nb) Lion\nc) Tiger\nd) Leopard\n"
+               "Which animal is known for its ability to glide through the air using skin flaps between its "
+               "limbs?\na) Flying squirrel\nb) Bat\nc) Sugar glider\nd) Flying fish\n"]
+
+    ani_ans = ["a: Zebra", "a: Parrot", "a: Elephant", "a: Chameleon", "a: Hummingbird", "a: Giraffe",
+               "a: Galapagos tortoise", "a: Beaver", "a: Cheetah", "b: Bat"]
+
+    Programming = ["Which programming language is often used for developing web applications?\na) Python\nb) Java\nc) "
+                   "HTML\nd) C++\n",
+                   "What does CSS stand for in web development?\na) Creative Style Sheets\nb) Computer Style "
+                   "Sheets\nc) Cascading Style Sheets\nd) Coding Style Sheets\n",
+                   "Which data structure follows the Last-In-First-Out (LIFO) principle?\na) Queue\nb) Stack\nc) "
+                   "Array\nd) Linked List\n",
+                   "Which programming language is commonly used for creating Android applications?\na) Swift\nb) "
+                   "Ruby\nc) Kotlin\nd) PHP\n",
+                   "Which keyword is used to declare a variable that cannot be changed once it is assigned a "
+                   "value?\na) static\nb) final\nc) const\nd) let\n",
+                   "Which programming paradigm is characterized by representing problems as a series of mathematical "
+                   "functions?\na) Object-oriented programming\nb) Procedural programming\nc) Functional "
+                   "programming\nd) Event-driven programming\n",
+                   "Which operator is used for concatenating strings in many programming languages?\na) +\nb) -\nc) "
+                   "*\nd) /\n",
+                   "What is the term used for finding and fixing errors in a program?\na) Debugging\nb) Compiling\nc) "
+                   "Optimizing\nd) Analyzing\n",
+                   "Which programming language is often used for data analysis and statistical computing?\na) "
+                   "JavaScript\nb) R\nc) C#\nd) Swift\n",
+                   "Which type of loop repeats a block of code a specific number of times?\na) For loop\nb) While "
+                   "loop\nc) Do-while loop\nd) Switch loop\n"]
+
+    prog_ans = ["c: HTML", "c: Cascading Style Sheets", "b: stack", "c: Kotlin", "b: final",
+                "c: Functional programming", "a: +", "a: Debugging", "b: R", "a: For loop"]
+
+
+class Quiz:
+    current_List = []
+    current_answers = []
+
+    total_score = 0
+    round = 0
+
+    @staticmethod
+    def __display_intro():
+        print("                           Welcome to the MJ Quiz Game                                  ")
+        print("You have to choose from given Genres for Quiz")
+        print("Each genre has 10 questions")
+
+    @staticmethod
+    def __choose_genre():
+        pf_genre = int(input("\n1. General Knowledge\n"
+                             "2. Music\n"
+                             "3. Movies\n"
+                             "4. Animals and Birds\n"
+                             "5. Programming\nGive answer in num: \n"))
+        return pf_genre
+
+    def __assign_List(self, genre):
+        quest = Questions()
+        if genre == 1:
+            self.current_List = quest.GK_Questions
+            self.current_answers = quest.gk_answers
+        elif genre == 2:
+            self.current_List = quest.Music
+            self.current_answers = quest.mk_ans
+        elif genre == 3:
+            self.current_List = quest.Movies
+            self.current_answers = quest.mov_ans
+        elif genre == 4:
+            self.current_List = quest.Animals
+            self.current_answers = quest.ani_ans
+        elif genre == 5:
+            self.current_List = quest.Programming
+            self.current_answers = quest.prog_ans
+
+    def start_game(self):
+        self.total_score = 0
+        if self.round == 0:
+            self.__display_intro()
+            self.round += 1
+        genre = self.__choose_genre()
+        self.__assign_List(genre)
+        self.__display_next_question(0)
+
+    def __continue_game(self):
+        response = input("Do You want to continue playing this game!: ")
+        if response in ('y', "Y", 'yes', 'YES', "Yes"):
+            self.start_game()
+        else:
+            print("Thank You for Playing!\nHave a nice day\nKeep Learning\nKeep Improving :)")
+
+    def __show_result(self, num):
+        answer = input("Choose option: ")
+        if answer == self.current_answers[num][0]:
+            print("Congratulation! You got the right answer\n")
+            self.total_score += 1
+        else:
+            print(":( Your answer is Wrong")
+            print(f"The correct answer is : {self.current_answers[num]}\n")
+
+        if num < 9:
+            self.__display_next_question(num + 1)
+        else:
+            print("\nYour total score is: ", self.total_score)
+            self.__continue_game()
+
+    def __display_next_question(self, num):
+        print(f"{num + 1}: {self.current_List[num]}")
+        self.__show_result(num)
+
+
+def main():
+    my_quiz = Quiz()
+    my_quiz.start_game()
+
+
+main()
